4f59ff6cb4193d2c912073d25522d375a179d4b0
VW <wshong8282@gmail.com>
2023-02-21 05:03:06 +0800

    recep register

--- Tuition Centre/Class/Receptionist.cs
+++ Tuition Centre/Class/Receptionist.cs
@@ -4,53 +4,172 @@ using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Cryptography.Pkcs;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
 namespace Tuition_Centre.Class
 {
     internal class Recep
     {
+        private string stuUsername;
+        private string stuPw;
+        private string role;
+
+        private int levelId;
+        private string stuId;
+        private string stuName;
+        private string stuIcP;
+        private string stuEmail;
+        private string stuPhone;
+        private string stuAddress;
+        private string stuEnrollDate;
+        private string birthday;
+        private string studyCourse;
+        private string memo;
+
+        private string subName1;
+        private string subName2;
+        private string subName3;
+
+
         // The connection string to the database
         static SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["myCS"].ToString());
 
         //Constructor Overloading
         public Recep()
-        { }
-
-        public void Register(string sN, string sIC, string sE, string sC, string sA, string sMOE, string sLV, string sSub)
         {
-            con.Open();
-            string reg = "INSERT INTO studentinfo (studentName, studentIC/Pass, studentEmail, studentPhone, studentAddress, studentEnrollmentDate, levelId) VALUES (@studentName, @studentIC/Pass, @studentEmail, @studentPhone, @studentAddress, @studentEnrollMth, @levelId)";
-            SqlCommand cmd = new SqlCommand(reg, con);
-            cmd.Parameters.AddWithValue("@studentName", sN);
-            cmd.Parameters.AddWithValue("@studentIC/Pass", sIC);
-            cmd.Parameters.AddWithValue("@studentEmail", sE);
-            cmd.Parameters.AddWithValue("@studentPhone", sC);
-            cmd.Parameters.AddWithValue("@studentAddress", sA);
-            cmd.Parameters.AddWithValue("@studentEnrollmentDate", sMOE);
-            cmd.Parameters.AddWithValue("@levelId", sLV);
-            cmd.Parameters.AddWithValue("@levelId", sLV);
-            cmd.ExecuteNonQuery();
 
-            string getID = "SELECT studentDatabasedId('studentinfo')";
-            SqlCommand cmd2 = new SqlCommand(getID, con);
-            int studentDatabaseID = Convert.ToInt32(cmd2.ExecuteScalar());
-
-            //<<<<<<< HEAD
-            //int usersId = this.UsersId;
-            //=======
-            // int usersId = this.UsersId;
-            //>>>>>>> b210ce48ed86a5556db0a399a06686b3eeedd36e
-            string stuUserId = "INSERT INTO studentinfo (studenDatabaseID, usersId) VALUES (@studenDatabaseID, @usersId)";
-            SqlCommand cmd3 = new SqlCommand(stuUserId, con);
-            cmd3.Parameters.AddWithValue("@studenDatabaseID", studentDatabaseID);
-            // cmd3.Parameters.AddWithValue("@usersId", usersId);
-            cmd3.ExecuteNonQuery();
-
-            con.Close();
         }
-  
+
+        public Recep(string stuUsername, string stuPw, string role, int levelId, string stuId, string stuName, string stuIcP, string stuEmail, string stuPhone, string stuAddress, string stuEnrollDate, string birthday, string studyCourse, string memo, string subName1, string subName2, string subName3)
+        {
+            this.stuUsername = stuUsername;
+            this.stuPw = stuPw;
+            this.role = role;
+            this.levelId = levelId;
+            this.stuId = stuId;
+            this.stuName = stuName;
+            this.stuIcP = stuIcP;
+            this.stuEmail = stuEmail;
+            this.stuPhone = stuPhone;
+            this.stuAddress = stuAddress;
+            this.stuEnrollDate = stuEnrollDate;
+            this.birthday = birthday;
+            this.studyCourse = studyCourse;
+            this.memo = memo;
+            this.subName1 = subName1;
+            this.subName2 = subName2;
+            this.subName3 = subName3;
+        }
+
+        
+        // Method to Register into users, studentInfo, studentSubject database 
+        public string addStudent()
+        {
+            string status;
+
+            // Create a new SqlConnection object with the connection string and return the status as result
+            con.Open();
+
+            // Insert the value of username, password, role to the users database
+            SqlCommand cmdUsers = new SqlCommand("INSERT INTO users (username, password, role) VALUES (@username, @password, @role); SELECT SCOPE_IDENTITY();", con);
+            cmdUsers.Parameters.AddWithValue("@username", stuUsername);
+            cmdUsers.Parameters.AddWithValue("@password", stuPw);
+            cmdUsers.Parameters.AddWithValue("@role", role);
+            int usersId = Convert.ToInt32(cmdUsers.ExecuteScalar());
+
+            // Insert student's information to the studentInfo database
+            SqlCommand cmdStuInfo = new SqlCommand("INSERT INTO studentInfo (usersId, levelId, studentId, studentName, studentIcP, studentEmail, studentPhone, studentAddress, studentEnrollDate, birthday, studyCourse, memo) " +
+                                                 "VALUES (@usersId, @levelId, @stuId, @stuName, @stuIcP, @stuEmail, @stuPhone, @stuAddress, @stuEnrollDate, @birthday, @studyCourse, @memo); SELECT SCOPE_IDENTITY();", con);
+            cmdStuInfo.Parameters.AddWithValue("@usersId", usersId);
+            cmdStuInfo.Parameters.AddWithValue("@levelId", levelId);
+            cmdStuInfo.Parameters.AddWithValue("@stuId", stuId);
+            cmdStuInfo.Parameters.AddWithValue("@stuName", stuName);
+            cmdStuInfo.Parameters.AddWithValue("@stuIcP", stuIcP);
+            cmdStuInfo.Parameters.AddWithValue("@stuEmail", stuEmail);
+            cmdStuInfo.Parameters.AddWithValue("@stuPhone", stuPhone);
+            cmdStuInfo.Parameters.AddWithValue("@stuAddress", stuAddress);
+            cmdStuInfo.Parameters.AddWithValue("@stuEnrollDate", stuEnrollDate);
+            cmdStuInfo.Parameters.AddWithValue("@birthday", birthday);
+            cmdStuInfo.Parameters.AddWithValue("@studyCourse", studyCourse);
+            cmdStuInfo.Parameters.AddWithValue("@memo", memo);
+            int studentDatabaseId = Convert.ToInt32(cmdStuInfo.ExecuteScalar());
+
+            // Insert the subjectId(s) into the studentSubject database
+            if (!string.IsNullOrEmpty(subName1))
+            {
+                // Get the subjectId according to the subjectName the user choose (subject 1)
+                SqlCommand cmdSub1 = new SqlCommand("SELECT subjectId FROM subject WHERE subjectName = @subName", con);
+                cmdSub1.Parameters.AddWithValue("@subName", subName1);
+                int subId1 = -1;
+
+                using (SqlDataReader reader = cmdSub1.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        subId1 = Convert.ToInt32(reader["subjectId"]);
+                    }
+                }
+
+                // Insert the subjectId into the studentSubject database (subject 1)
+                SqlCommand cmdStuSub1 = new SqlCommand("INSERT INTO studentSubject (studentDatabaseId, subjectId) VALUES (@stuDbId, @subId)", con);
+                cmdStuSub1.Parameters.AddWithValue("@stuDbId", studentDatabaseId);
+                cmdStuSub1.Parameters.AddWithValue("@subId", subId1);
+                cmdStuSub1.ExecuteNonQuery();
+            }
+
+            if (!string.IsNullOrEmpty(subName2))
+            {
+                // Get the subjectId according to the subjectName the user choose (subject 2)
+                SqlCommand cmdSub2 = new SqlCommand("SELECT subjectId FROM subject WHERE subjectName = @subName", con);
+                cmdSub2.Parameters.AddWithValue("@subName", subName2);
+                int subId2 = -1;
+
+                using (SqlDataReader reader = cmdSub2.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        subId2 = Convert.ToInt32(reader["subjectId"]);
+                    }
+                }
+
+                // Insert the subjectId into the studentSubject database (subject 2)
+                SqlCommand cmdStuSub2 = new SqlCommand("INSERT INTO studentSubject (studentDatabaseId, subjectId) VALUES (@stuDbId, @subId)", con);
+                cmdStuSub2.Parameters.AddWithValue("@stuDbId", studentDatabaseId);
+                cmdStuSub2.Parameters.AddWithValue("@subId", subId2);
+                cmdStuSub2.ExecuteNonQuery();
+            }
+
+            if (!string.IsNullOrEmpty(subName3))
+            {
+                // Get the subjectId according to the subjectName the user choose (subject 3)
+                SqlCommand cmdSub3 = new SqlCommand("SELECT subjectId FROM subject WHERE subjectName = @subName", con);
+                cmdSub3.Parameters.AddWithValue("@subName", subName3);
+                int subId3 = -1;
+
+                using (SqlDataReader reader = cmdSub3.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        subId3 = Convert.ToInt32(reader["subjectId"]);
+                    }
+                }
+
+                // Insert the subjectId into the studentSubject database (subject 3)
+                SqlCommand cmdStuSub3 = new SqlCommand("INSERT INTO studentSubject (studentDatabaseId, subjectId) VALUES (@stuDbId, @subId)", con);
+                cmdStuSub3.Parameters.AddWithValue("@stuDbId", studentDatabaseId);
+                cmdStuSub3.Parameters.AddWithValue("@subId", subId3);
+                cmdStuSub3.ExecuteNonQuery();
+            }
+            con.Close();
+
+            status = "Registration Successful.";
+            return status;
+        }
+
 
         // Method to search for a student in the database and return the results as a DataTable
         public DataTable SearchStu(string searchName)
@@ -76,14 +195,9 @@ namespace Tuition_Centre.Class
             return dt;
         }
 
-        /*
-        public static object Register(Receptionist)
-        {
-            con.Open();
-            SqlCommand cmd = new SqlCommand("select * from students where studentName = @a", con);
-            con.Close()
-        }
 
+
+        /*
         public void UpdateStuSubject(Receptionist)
         {
 
@@ -93,55 +207,48 @@ namespace Tuition_Centre.Class
         {
 
         }
-
-        public void DeleteStudent(Receptionist)
-        {
-
-        }
-
-        public void UpdateRecepInfo(Receptionist)
-        {
-
-        }
-
-        public void GenerateReceipt(Receptionist)
-        {
-
-        }
-
-
-        public void SearchStu()
-        {
-            string status = null;
-            con.Open();
-
-            
-            SqlCommand cmd = new SqlCommand("select count(*) from studentInfo where studentName=@stuName", con);
-            cmd.Parameters.AddWithValue("@stuName", studentName);
-            SqlDataReader rd = cmd.ExecuteReader();
-
-            while (rd.Read())
-            {
-                lblStudentInfo.Text = rd.GetString(5) + " |  " + rd.GetString(4) + " |  " +  rd.GetString(12);
-            }
-
-            int count = Convert.ToInt32(cmd.ExecuteScalar().ToString());
-
-            if (count > 0)
-            {
-                SqlCommand cmd2 = new SqlCommand("select role from users where username=@a and password=@b", con);
-                cmd2.Parameters.AddWithValue("@a", username);
-                cmd2.Parameters.AddWithValue("@b", password);
-                string userRole = cmd2.ExecuteScalar().ToString();
-
-                if (userRole.Trim() == "admin")
-                {
-                    FrmMainAdmin a = new FrmMainAdmin(un);
-                    Main.ActiveForm.Hide();
-                    a.ShowDialog();
-                }
-            }
-        }
         */
+
+        public string deleteStudent()
+        {
+            string status;
+            string searchName = null;
+            int stuDbId = 0;
+
+            con.Open();
+            
+            // Delete the student's subjects first
+            SqlCommand cmdDeleteStudentSubjects = new SqlCommand("DELETE FROM studentSubject WHERE studentDatabaseId = (SELECT studentDatabaseId FROM studentInfo WHERE studentName = @studentName)", con);
+            cmdDeleteStudentSubjects.Parameters.AddWithValue("@studentName", searchName);
+            cmdDeleteStudentSubjects.Parameters.AddWithValue("@stuDbId", stuDbId);
+            cmdDeleteStudentSubjects.ExecuteNonQuery();
+
+            // Delete the student's user account next
+            SqlCommand cmdDeleteStudentUser = new SqlCommand("DELETE FROM users WHERE username = @studentName", con);
+            cmdDeleteStudentUser.Parameters.AddWithValue("@studentName", searchName);
+            cmdDeleteStudentUser.ExecuteNonQuery();
+
+            // Delete the student's information last
+            string deleteStudentInfoQuery = "DELETE FROM studentInfo WHERE studentName = @studentName";
+            SqlCommand cmdDeleteStudentInfo = new SqlCommand(deleteStudentInfoQuery, con);
+            cmdDeleteStudentInfo.Parameters.AddWithValue("@studentName", searchName);
+            cmdDeleteStudentInfo.ExecuteNonQuery();
+
+            status = "Student information has been deleted successfully.";
+            return status;
+        }
+
+        /*
+            public void UpdateRecepInfo(Receptionist)
+            {
+
+            }
+
+            public void GenerateReceipt(Receptionist)
+            {
+
+            }
+
+        }*/
     }
 }
--- Tuition Centre/Receptionist/Student Info 3.cs
+++ Tuition Centre/Receptionist/Student Info 3.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tuition_Centre.Class;
 
 namespace Tuition_Centre.Receptionist
 {
@@ -15,6 +16,14 @@ namespace Tuition_Centre.Receptionist
         public frmUpdateStu3()
         {
             InitializeComponent();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            Recep rcp = new Recep();
+            string searchName = "ab";
+            MessageBox.Show(rcp.deleteStudent());
+
         }
     }
 }
